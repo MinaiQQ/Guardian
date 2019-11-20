250d9b2183916aa0158d92564f2b20da0aeef74b
Joshua Lewis <Darkwater409@gmail.com>
2019-11-20 02:22:50 -0700

    Update guild.js

--- commands/guild.js
+++ commands/guild.js
@@ -8,13 +8,16 @@ module.exports = {
            
             
             const embed = new Discord.RichEmbed()
+            .setTitle("Guild Info")
             .setColor(0x00AE86)
             .setFooter("Guardian, phantomdev-github/Guardian", "https://raw.githubusercontent.com/phantomdev-github/Resources/master/images/Bots/Guardian/avatar.png")
             .setImage("https://raw.githubusercontent.com/phantomdev-github/Resources/master/images/Branding/banner.png")
             .setThumbnail("https://raw.githubusercontent.com/phantomdev-github/Resources/master/images/Branding/avatar.png")
             .setTimestamp()
             .setURL("https://www.github.com/phantomdev-github/Guardian")
-            .addField("Guild Info",
-            (`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`))
+            .addField("Server Name",
+            (`${message.guild.name}`))
+            .addField("Total Members",
+            (`${message.guild.memberCount}`))
     message.channel.send({embed});
 }};
