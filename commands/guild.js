b1a410af89191ca0b1f5f080eac5f9fdf358f791
Joshua Lewis <Darkwater409@gmail.com>
2019-11-26 19:05:35 -0700

    Update guild.js

--- commands/guild.js
+++ commands/guild.js
@@ -11,8 +11,6 @@ module.exports = {
             .setTitle("Guild Info")
             .setColor(0x00AE86)
             .setFooter("Guardian, phantomdev-github/Guardian", "https://raw.githubusercontent.com/phantomdev-github/Resources/master/images/Bots/Guardian/avatar.png")
-            .setImage("https://raw.githubusercontent.com/phantomdev-github/Resources/master/images/Branding/banner.png")
-            .setThumbnail("https://raw.githubusercontent.com/phantomdev-github/Resources/master/images/Branding/avatar.png")
             .setTimestamp()
             .setURL("https://www.github.com/phantomdev-github/Guardian")
             .addField("Server Name",
