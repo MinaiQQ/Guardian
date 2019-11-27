dce486e30ce16fa23f22794cf781658da20bc5b1
Joshua Lewis <Darkwater409@gmail.com>
2019-11-26 20:45:17 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,9 @@ module.exports = {
     description: 'client on ready event.',
     execute:function(client) {
         client.on("ready", () => {
-            client.user.setActivity(`g!help on ${client.guilds.size} servers`, { type: 'LISTENING' });
-            console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
+           client.user.setActivity(`g!help | ${client.guilds.size} servers`, {
+           type: "STREAMING",
+           url: "https://www.twitch.tv/hero_nimbi"
+});            console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 		})
 }};
